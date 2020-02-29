54d81d71eb91f4a2d146a4096d820476c79af335
Kiran Pradhan <kpradhan@uw.edu>
2020-02-29 13:08:16 -0800

    SM data scientist

--- src/Containers/SMLessonPage/SMLessonPage.js
+++ src/Containers/SMLessonPage/SMLessonPage.js
@@ -16,7 +16,7 @@ class SMLessonPage extends React.Component {
           <div id="SMLessonPage_introduction"> 
             <div id="SMLessonPage_introduction_Text">
                 <header> Let's Talk about Targeted Social Media Ads </header>
-                <p> Social Media utilize advertisements to keep themselves afloat. From your favorite personality on 
+                <p> Social Media utilize advertisements to keep themselves afloat. From your favorite     personality on 
                     Facebook, Instagram, TikTok, or Youtube, to the actual companies themselves, the synergy from 
                     advertisers getting exposure and people/companies getting funded continues to help both groups out. 
                     Let’s look into how social media advertisements can get to you, and how that can work in your 
@@ -26,7 +26,16 @@ class SMLessonPage extends React.Component {
               <img src={SocialDashboard} alt="Social Media Dashboard" class="socialMediaDashboardImage"></img>
             </div>
           </div>
-
+          
+          <div id="SMLessonPage_datascientist">
+            <header>
+              <p class="title"> Our Data Scientist is on it </p>
+            </header>
+            <span class="dot1"></span>
+            <span class="dot2"></span>
+            <span class="dot3"></span>
+            <p>While we're analyzing your ad words, let's simulate an ad campaign to show how far simple advertisements can spread through a social media platform by targeting people with specific interests</p>
+          </div>
 
         </div>
     )}
